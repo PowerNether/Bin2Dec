d5380c26aa67a5f74ef33e7eb9fb0fbbe393a796
PowerNether <64768544+PowerNether@users.noreply.github.com>
2020-06-17 01:51:59 +0500

    master

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 function bin2dec() {
     var bin = document.getElementById('bin').value;
-    if (bin !== "") {
-        bin.split('').map((char) => {
-            if (char == "0" || char == "1") {
+    if (bin === "") return alert("Введите значение!");
+    bin.split('').map((char) => {
+            if (char === "0" || char === "1") {
                 var dec = parseInt(bin, 2);
                 document.getElementById('dec').value = dec;
             }
             else {
                 alert("Вводите только двоичное число (101)!");
-                return false;
             }
         });
-    }
-    else {
-        return alert("Введите значение!");
-    }
 }
